58fc971f4557f70adb553c0da3feda01624658c1
rtumalle0518 <rtumalle9700@gmail.com>
2021-03-14 15:49:30 -0400

    add playRound function

--- app.js
+++ app.js
@@ -1,16 +1,53 @@
 function computerPlay(){
     let x = Math.floor(Math.random()*Math.floor(3));
     if (x === 0){
-        console.log("rock");
+        return 'rock';
     } else if ( x === 1){
-        console.log("paper");
+        return 'paper';
     } else {
-        console.log("scissor")
+        return 'scissor';
+    }
+}
+function playToInt(play){
+    if (play === 'rock') {
+        return 0;
+    } else if (play === 'paper') {
+        return 1;
+    } else {
+        return 2;
+    }
+}
+function intToPlay(play){
+    if (play === 0) {
+        return 'rock';
+    } else if (play === 1) {
+        return 'paper';
+    } else {
+        return 'scissor';
     }
 }
 function playRound(playerSelection, computerSelection){
+    const x = playerSelection.toLowerCase();
+
+    const player = playToInt(x);
+    const computer = playToInt(computerSelection);
+
+    const win = [
+        [2, 0, 0],
+        [1, 2, 1],
+        [1, 0, 2]
+    ];
     
+    if (win[player][computer] === 2) {
+        return 'It\'s a draw!';
+    } else if (win[player][computer] === 1) {
+        return `You Lose! ${computerSelection} beats ${x}`;
+    } else if (win[player][computer] === 0) {
+        return `You win! ${x} beats ${computerSelection}`;
+    }
 }
+
+
 
 
 
