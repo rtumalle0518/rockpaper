7b3268d54e090af00e2cc0dd873247af9626ddfe
rtumalle0518 <rtumalle9700@gmail.com>
2021-03-14 14:41:43 -0400

    Added computerPlay() and playRound()

---
+++ app.js
@@ -0,0 +1,16 @@
+function computerPlay(){
+    let x = Math.floor(Math.random()*Math.floor(3));
+    if (x === 0){
+        console.log("rock");
+    } else if ( x === 1){
+        console.log("paper");
+    } else {
+        console.log("scissor")
+    }
+}
+function playRound(playerSelection, computerSelection){
+    
+}
+
+
+
